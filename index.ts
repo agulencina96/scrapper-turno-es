001ac9eb680813d1c45a1ef4d4e9c4ba176561b8
Agustin Lencina <agulencina96@gmail.com>
2023-04-03 12:25:16 -0300

    take sensitive info from .env file

--- index.ts
+++ index.ts
@@ -1,71 +1,72 @@
-import puppeteer from 'puppeteer';
-import { sendMail } from './sendMail';
-import * as dotenv from 'dotenv'
-dotenv.config()
+import puppeteer from "puppeteer";
+import { sendMail } from "./sendMail";
 
 async function main() {
-    setInterval(async() => {
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-    
-        await page.goto('https://www.exteriores.gob.es/Consulados/cordoba/es/ServiciosConsulares/Paginas/index.aspx?scco=Argentina&scd=129&scca=Pasaportes+y+otros+documentos&scs=Pasaportes+-+Requisitos+y+procedimiento+para+obtenerlo');
-    
-        const textSelector = await page.waitForSelector(
-            '#ctl00_ctl45_g_c3a6084e_3cf9_4caf_a4e6_3d59225c8dc3 > div > section > div > div > p:nth-child(21) > a'
-        );
-        if (!textSelector) {
-            throw new Error('No se encontró el selector de texto');
-        }
-        await textSelector.click();
-        let pages = await browser.pages();
-    
-        while (pages.length < 3) {
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            pages = await browser.pages();
-        }
-    
-        const page2 = pages[2];
-        await page2.bringToFront();
-        const aceptarSelector = await page2.waitForSelector(
-            '#bktContinue > div'
-        );
-    
-        if (!aceptarSelector) {
-            throw new Error('No se encontró el selector de aceptar');
-        }
-        await aceptarSelector.evaluate((node) => node.click());
-        const opcionPasaporteSelector = await page2.waitForSelector(
-            '#idListServices > a > div > div.clsBktServiceDescription',{
-                timeout: 0
-            }
-        );
-    
-        if (!opcionPasaporteSelector) {
-            throw new Error('No se encontró el selector de pasaporte');
-        }
-    
-        await opcionPasaporteSelector.click();
-    
-        const noHayTurnosSelector = await page2.waitForSelector(
-            '#idDivNotAvailableSlotsTextTop',{
-                timeout: 0
-            }
-        );
-    
-        if (!noHayTurnosSelector) {
-             await sendMail();
-             console.log('HAY TURNOS')
-        }else{
-            console.log(Date.now(),'NO HAY TURNOS')
-        }
+  try {
+    console.log("Iniciando script a las: ", new Date().toLocaleString());
+    setInterval(async () => {
+      console.log("Nuevo intento a las: ", new Date().toLocaleString());
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
+      await page.goto(
+        "https://www.exteriores.gob.es/Consulados/cordoba/es/ServiciosConsulares/Paginas/index.aspx?scco=Argentina&scd=129&scca=Pasaportes+y+otros+documentos&scs=Pasaportes+-+Requisitos+y+procedimiento+para+obtenerlo"
+      );
 
-            await browser.close();
-        } catch (error) {
-            console.error(error);
+      const textSelector = await page.waitForSelector(
+        "#ctl00_ctl45_g_c3a6084e_3cf9_4caf_a4e6_3d59225c8dc3 > div > section > div > div > p:nth-child(21) > a"
+      );
+      if (!textSelector) {
+        throw new Error("No se encontró el selector de texto");
+      }
+      await textSelector.click();
+      let pages = await browser.pages();
+
+      while (pages.length < 3) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        pages = await browser.pages();
+      }
+
+      const page2 = pages[2];
+      await page2.bringToFront();
+      const aceptarSelector = await page2.waitForSelector("#bktContinue > div");
+
+      if (!aceptarSelector) {
+        throw new Error("No se encontró el selector de aceptar");
+      }
+      await aceptarSelector.evaluate((node) => node.click());
+      const opcionPasaporteSelector = await page2.waitForSelector(
+        "#idListServices > a > div > div.clsBktServiceDescription",
+        {
+          timeout: 0,
         }
-    }, 10 * 60 * 1000);
+      );
+
+      if (!opcionPasaporteSelector) {
+        throw new Error("No se encontró el selector de pasaporte");
+      }
+
+      await opcionPasaporteSelector.click();
+
+      const noHayTurnosSelector = await page2.waitForSelector(
+        "#idDivNotAvailableSlotsTextTop",
+        {
+          timeout: 0,
+        }
+      );
+
+      if (!noHayTurnosSelector) {
+        await sendMail();
+        console.log(new Date().toLocaleString(), "HAY TURNOS!!!!!!!!");
+      } else {
+        console.log(new Date().toLocaleString(), "NO HAY TURNOS");
+      }
+      await browser.close();
+    }, 2 * 60 * 1000);
+  } catch (error) {
+    console.error("Error: ", new Date().toLocaleString());
+    console.error(error);
+  }
 }
 
-main()
+main();
